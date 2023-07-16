24f995d48af19d2c64a0b5cf23db22fef2409fe0
Anas <anashamed12p@gmail.com>
2023-07-16 05:14:42 -0700

    Mandatory task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,29 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - main block
  * Description: print the alphabet (lower and upper case) using putchar
  * and if it is positive, negative, or zero
  * Return: 0
  */
-
 int main(void)
 {
-	
-   for (char alpha = 'a'; alpha <= 'z'; alpha++)
+char alpha='a';
+   while (alpha<='z')
    {
-    putchar(alpha);
+      putchar(alpha);
+      alpha++;
    }
+   
+alpha='A';
 
-   for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+   while (alpha<='Z')
    {
-    putchar(alpha);
-   }
+      putchar(alpha);
+      alpha++;
 
-   putchar('\n');
+   }
+putchar('\n');
    
     
-	return (0);
+return (0);
 }
