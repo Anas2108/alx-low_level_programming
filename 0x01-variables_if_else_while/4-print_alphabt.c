f593b306c4adb5459f03fed206ce66789db891a8
Anas <anashamed12p@gmail.com>
2023-07-16 06:03:42 -0700

    Mandatory task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print the alphabet (except q and e) using putchar
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main()
+{
+char alpha = 'a';
+while (alpha <= 'z')
+{
+if (alpha != 'q' && alpha != 'e')
+putchar(alpha);
+   
+alpha++;
+}
+putchar('\n');
+return(0);
+}
