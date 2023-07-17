065748e08218db3a3bae483effd68916363943b9
Anas <anashamed12p@gmail.com>
2023-07-17 15:17:49 -0700

    Mandatory task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print the alphabet (except q and e) using putchar
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+int num = 0;
+while (num<10)
+{
+printf("%i",num);
+num++;
+}
+putchar('\n');
+}
