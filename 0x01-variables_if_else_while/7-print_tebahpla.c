df4512c4aee2ed9c82484c4b096ac96cb1a21654
Anas <anashamed12p@gmail.com>
2023-07-17 15:34:58 -0700

    Mandatory task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description:prints the lowercase alphabet in reverse, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+
+	putchar('\n');
+	return (0);
+}
