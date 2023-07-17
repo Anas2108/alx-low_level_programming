99190f83d14c87b0e10b43f0cd6246597bc9ee90
Anas <anashamed12p@gmail.com>
2023-07-17 15:51:28 -0700

    Mandatory task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print numbers from 0 to 9 using putchar
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+int num = 48;
+while (num <= 57)
+{
+putchar((char)num);
+if(num!=57)
+{
+putchar(',');
+putchar(' ');
+}
+num++;
+}
+putchar('\n');
+return (0);
+}
