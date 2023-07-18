c8b2142ad47b36b10b0a15d64fce9e82238574d3
Anas <anashamed12p@gmail.com>
2023-07-18 11:56:19 -0700

    Mandatory task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print the alphabet (except q and e) using putchar
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main (void)
+{
+printf("_putchar\n");
+return (0);
+}
