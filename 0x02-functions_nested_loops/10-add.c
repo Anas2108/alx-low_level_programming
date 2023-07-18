630d66b178c0237e5e121352637375410c106980
Anas <anashamed12p@gmail.com>
2023-07-18 14:39:44 -0700

    Mandatory task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - returns the sum of its parameters
+ * @num1: int type number
+ * @num2: int type number
+ * Return:0
+ */
+
+
+int add(int num1, int num2)
+{
+return (num2 + num1);
+}
