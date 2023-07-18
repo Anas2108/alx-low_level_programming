ad4d38975a32d0765e307d78526d18e76ad93a2f
Anas <anashamed12p@gmail.com>
2023-07-18 13:14:21 -0700

    Mandatory task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
+ */
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+int counter=10;
+while(counter>0){
+while (alpha <= 'z')
+{
+putchar(alpha);
+alpha++;
+}
+putchar('\n');
+counter--;
+alpha='a';
+}
+}
