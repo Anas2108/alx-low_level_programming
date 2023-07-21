ab7188b0366303d125f7424e47e2d2e7d0f002c4
Anas <anashamed12p@gmail.com>
2023-07-21 07:23:43 -0700

    Mandatory task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,7 +5,7 @@
  * @c:  is the int that will use for the argument of the function
  * Return: 1 if digit  0 if not
  */
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
