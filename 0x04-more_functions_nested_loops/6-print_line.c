194998a75ad225adad98b5c2d31a9ee34320f5b1
Anas <anashamed12p@gmail.com>
2023-07-21 10:38:36 -0700

    Mandatory task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
+
+void print_line(int n)
+{
+for (int i = 0;i < n;i++)
+{
+putchar('_');
+}
+putchar('\n');
+}
