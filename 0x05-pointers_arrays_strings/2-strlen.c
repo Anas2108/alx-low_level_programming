7ecd4fc1c9e4580a0efd37de96649ac6d81076a1
Anas <anashamed12p@gmail.com>
2023-07-24 08:39:12 -0700

    Mandatory task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen -function that calc the length of string
+ * @s:  pointer
+ * Return: the length of string
+ */
+
+int _strlen(char *s)
+{
+int counter = 0;
+while (s[counter]!='\0')
+{
+counter++;
+}
+return (counter);
+
+}
