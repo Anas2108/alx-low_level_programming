f3cfeb8031212914ac5e9f4897cafb3590311f1e
Anas <anashamed12p@gmail.com>
2023-07-24 11:06:43 -0700

    Mandatory task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev -function that reverse the string
+ * @s:  pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+int counter = 0;
+int i=0;
+int half;
+char spare;
+while (s[counter] != '\0')
+{
+counter++;
+}
+half = counter / 2;
+while (half--)
+{
+spare = s[counter - i - 1];
+s[counter - i - 1] = s[i];
+s[i] = spare;
+i++;
+}
+}
