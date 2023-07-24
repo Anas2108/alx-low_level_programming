88cd1236f9f58bd42e800df3b5bdb3d158d6b853
Anas <anashamed12p@gmail.com>
2023-07-24 12:02:17 -0700

    Mandatory task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - function that prints the array
+ * @a:  pointer
+ * @n:  number of elements to be printed
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0 ; i < n ; i++)
+{
+if (i != (n - 1))
+printf("%i, ",a[i]);
+else
+printf("%i",a[i]);
+}
+putchar('\n');
+}
+
