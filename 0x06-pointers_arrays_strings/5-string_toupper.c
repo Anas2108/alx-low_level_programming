b4d173a8628516e82b171b557cae2469d6ebd8dc
Anas <anashamed12p@gmail.com>
2023-07-27 16:27:22 -0700

    Mandatory task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - reverse a int array
+ * @a: pointer
+ * Return: char*
+*/
+char* string_toupper(char *a)
+{
+int i;
+for (i = 0; a[i] != '\0'; i++)
+{
+if (a[i] >= 97 && a[i] <= 122)
+a[i] -= 32;
+}
+return (a);
+}
+
