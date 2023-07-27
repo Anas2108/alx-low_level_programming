e34f492d1c4465740a91adb3b722b41887e2f518
Anas <anashamed12p@gmail.com>
2023-07-27 14:35:45 -0700

    Mandatory task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
+ * _strncat - concatenates two strings
  * @dest:  pointer point to the first string
  * @src:  Pointer point to the second string
  * @n:    number of letters 
@@ -11,12 +11,9 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int counter = 0, src_counter = 0, i;
-
 for (i = 0; dest[i] != '\0'; i++);
-
 if (n > i)
 n = i;
-
 while (dest[counter] != '\0')
 {
 counter++;
@@ -27,6 +24,6 @@ dest[counter] = src[src_counter];
 src_counter++;
 counter++;
 }
-
 return (dest);
 }
+
