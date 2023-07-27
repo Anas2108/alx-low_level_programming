76daa6c3226ed5bad8c14a00609a0608ac137909
Anas <anashamed12p@gmail.com>
2023-07-27 14:02:51 -0700

    Mandatory task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest:  pointer point to the first string
+ * @src:  Pointer point to the second string
+ * Return: char*
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char *spare;
+int counter=0,src_counter=0;
+
+while (dest[counter] != '\0')
+{
+counter++;
+}
+while (src[src_counter] != '\0')
+{
+dest[counter]=src[src_counter];
+src_counter++;
+counter++;
+}
+return (dest);
+}
