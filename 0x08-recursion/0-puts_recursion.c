53f5d9b7de1635594977c5d7a094efb00f63c9c7
Anas <anashamed12p@gmail.com>
2023-08-02 18:25:46 -0700

    Mandatory task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_puts_recursion -function that prints a string, followed by a new line
+ * @s:  pointer 
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+
+if (*s =='\0')
+return;
+putchar(*s);
+_puts_recursion(s+1);
+}
