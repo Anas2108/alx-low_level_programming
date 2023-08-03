87e0c5ccca408b36f6daabf4b448fd10880fb9b5
Anas <anashamed12p@gmail.com>
2023-08-02 18:39:57 -0700

    Mandatory task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string in reverse, followed by a new line
+ * @s: pointer to the string
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+    if (*s > '\0')
+    {
+        _print_rev_recursion(s+1);
+        putchar(*s);
+    }
+}
+
+
