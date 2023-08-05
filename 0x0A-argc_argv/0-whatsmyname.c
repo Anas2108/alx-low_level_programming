4983b823928a6f8d6cc967e58e584fd3f351341a
Anas <anashamed12p@gmail.com>
2023-08-04 17:37:05 -0700

    Mandatory task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+int main (int argc ,char const *argv[])
+{
+    (void)argc;
+    printf ("%s",argv[0]);
+    return (0);
+}
