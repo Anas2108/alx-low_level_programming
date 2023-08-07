11d38a2e48f60e2c28951f09607073428f56ba0f
Anas <anashamed12p@gmail.com>
2023-08-07 10:17:42 -0700

    Mandatory task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *create_array - array for prints a string
+ *@size: number elements array
+ *@c: char
+ *Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *ar;
+int i;
+ar = malloc (size * sizeof(char));
+if (size == 0)
+return NULL;
+
+if (ar == 0)
+return NULL;
+
+for (i=0; i < size; i++)
+{
+ar[i]=c;
+}
+
+return (ar);
+}
