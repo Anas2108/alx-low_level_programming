6b0673a5195559d12fead7f7d7786ac4aaa83f30
Anas <anashamed12p@gmail.com>
2023-08-14 16:07:05 -0700

    Mandatory task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,13 +15,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *new;
 dog_t new_dog;
 new = &new_dog;
-if (!new)
-{
-return (NULL);
-}
+if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 new->age = age;
 new->name = name;
+if (new->name == NULL)
+	{
+		free(new->name);
+		free(new);
+		return (NULL);
+	}
 new->owner = owner;
+if (new->owner == NULL)
+	{
+		free(new->owner);
+		free(new);
+		return (NULL);
+	}
 return (new);
 }
-
