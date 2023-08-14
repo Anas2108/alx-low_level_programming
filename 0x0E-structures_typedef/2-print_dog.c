b0934237d29bc1eeb00fbc4b20d2297f684239f9
Anas <anashamed12p@gmail.com>
2023-08-14 15:22:24 -0700

    Mandatory task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,18 +8,24 @@
 
 void print_dog(struct dog *d)
 {
-if (d)
-{
-if (d->name == NULL)
-printf("Name: (nil)");
-else
-printf("Name: %s\n",d->name);
-
-printf("Age: %f\n",d->age);
-
-if (d->owner == NULL)
-printf("Name: (nil)");
-else
-printf("Owner: %s\n",d->owner);
-}
+	if (d)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		printf("Age: %f\n", d->age);
+		if (d->owner)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+	}
 }
