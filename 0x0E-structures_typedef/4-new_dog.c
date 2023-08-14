c84072cf91ce99cf88d840103989f54ed1d5467a
Anas <anashamed12p@gmail.com>
2023-08-14 15:47:03 -0700

    Mandatory task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - new dog
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: newdog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new;
+new->age = age;
+new->name = name;
+new->owner = owner;
+return (new);
+}
