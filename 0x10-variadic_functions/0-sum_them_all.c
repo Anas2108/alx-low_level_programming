013548e00dbb6aa7bbf8a61115da8734782a741e
Anas <anashamed12p@gmail.com>
2023-08-18 17:09:09 -0700

    Mandatory task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,18 +1,24 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all -  sum all arguments
+ * @n: int, number of undefined arguments
+ *
+ * Return: sum of args
+ * On error, 0 is returned
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
-	int i;
-	va_list num;
-	va_start (num,n);
+va_list list;
+unsigned int i, sum = 0;
 
-	for (i = 0; i < n; i++)
-	sum += va_arg (num, int);
+va_start(list, n);
+if (n != 0)
+	for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
+	;
+va_end(list);
 
-	va_end (num);
-
-	return (sum);
-
-
+return (sum);
 }
