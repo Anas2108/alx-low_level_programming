f7dd73d6c5b41c6d8c33c7b2baa399c433dc45ac
Anas <anashamed12p@gmail.com>
2023-08-18 17:07:09 -0700

    Mandatory task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+	va_list num;
+	va_start (num,n);
+
+	for (i = 0; i < n; i++)
+	sum += va_arg (num, int);
+
+	va_end (num);
+
+	return (sum);
+
+
+}
